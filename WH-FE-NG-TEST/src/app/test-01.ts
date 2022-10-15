7be1d8494c3efa15ba689eb7d2e2be51acbe0e7b
Michael Chilaka <obichilaka@gmail.com>
2022-10-15 09:10:27 +0100

    For Angular, test-01.ts completed

--- WH-FE-NG-TEST/src/app/test-01.ts
+++ WH-FE-NG-TEST/src/app/test-01.ts
@@ -5,33 +5,59 @@
  * * Both [monthly_payment] and [late_payment] should print in the template in currency format : $1,234
  */
 
-import { Component, Input,NgModule  } from '@angular/core';
+import { Component, Input, NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 
 @Component({
-    selector : 'ng-app',
-    template : `<div>
-                    <h2>Loan Details</h2>
-                    <b>Monthly Payment:</b> {{monthly_payment}} <br/>
-                    <b>Late Payment Fee : {{late_payment}}</b> <br/>
-                </div>`
+  selector: "ng-app",
+  template: `<div>
+    <h2>Loan Details</h2>
+    <b>Monthly Payment:</b> {{ monthly_payment | currency}} <br />
+    <b>Late Payment Fee : {{ late_payment | currency}}</b> <br />
+  </div>`,
 })
 export class Test01Component {
+  loan_amount: number = 0;
+  monthly_payment: number = this.loan_amount * 0.02;
+  late_payment = this.monthly_payment * 0.05;
+}
 
-    loan_amount:number = 1000;
-    monthly_payment:number = 200;
-    late_payment = 10;
+import { Pipe, PipeTransform } from "@angular/core";
+import { formatCurrency, getCurrencySymbol } from "@angular/common";
+
+@Pipe({
+    name: 'currency'
+})
+export class CurrencyComponent implements PipeTransform {
+  transform(
+    value: number,
+    currencyCode: string = "USD",
+    display: "code" | "symbol" | "symbol-narrow" | string | boolean = "symbol",
+    numbersInfo: string = "1.2-2",
+    locale: string = "en"
+  ): string | null {
+    if (isNaN(Number(value)) || value === 0) {
+      return "N/A";
+    }
+    return formatCurrency(
+      value,
+      locale,
+      getCurrencySymbol(currencyCode, "wide"),
+      currencyCode,
+      numbersInfo
+    );
+  }
 }
 
 @NgModule({
-    imports : [
-        RouterModule.forChild([
-            {
-                path : "",
-                component : Test01Component
-            }
-        ])
-    ],
-    declarations : [Test01Component]
+  imports: [
+    RouterModule.forChild([
+      {
+        path: "",
+        component: Test01Component,
+      },
+    ]),
+  ],
+  declarations: [Test01Component, CurrencyComponent],
 })
 export class Test01Module {}
