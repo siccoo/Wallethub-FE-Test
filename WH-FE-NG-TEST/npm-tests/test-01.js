e41dcec2cf1f87df357d35c6c381b990c8cc3729
Michael Chilaka <obichilaka@gmail.com>
2022-10-15 22:44:40 +0100

    For Node.js, test-01.js completed

--- WH-FE-NG-TEST/npm-tests/test-01.js
+++ WH-FE-NG-TEST/npm-tests/test-01.js
@@ -7,40 +7,56 @@
  */
 
 import fs from "fs-extra";
-import {fastify } from "fastify";
+import { fastify } from "fastify";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const data = { error : false, users : ["John Doe","Lucita Esau", "Thomas Friedman", "Norma Helms", "Amy Manning"]  };
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+const data = {
+    error: false,
+    users: [
+        "John Doe",
+        "Lucita Esau",
+        "Thomas Friedman",
+        "Norma Helms",
+        "Amy Manning"
+    ],
+};
 
 // write the json saving code here
+const filedPath = `${__dirname}/test-01.json`;
+await fs.writeFile(filedPath, JSON.stringify(data));
 
 
 const app = fastify({
-    ignoreTrailingSlash : true,
-    keepAliveTimeout : 65 * 1000
+    ignoreTrailingSlash: true,
+    keepAliveTimeout: 65 * 1000
 });
 
-app.get('/',(request,reply)=>{
-    
-
+app.get('/', (request, reply) => {
     reply.header('Content-Type', 'text/html; charset=utf-8');
     // read the json here and insert the list names into the html
-
-        const page = 
+    const dataFromFile = fs.readFileSync(filedPath, "utf-8");
+    const users = JSON.parse(dataFromFile).users;
+    const page =
         `<html>
             <head>
                 <title>Wallethub Test</title>
             </head>
             <body>
-            <p>..print the list here..</p>
+            ${users.map((user) => `<p>${user}</p>`).join("")}
             </body>
         </html>`;
-        
-        reply.send(page);
-    
-    
+
+    reply.send(page);
+
+
 });
 
 // server start
-app.listen(8080,"0.0.0.0").then((address)=>{
+app.listen(8080, "0.0.0.0").then((address) => {
     console.log(`Server started at ${address}`);
 });
